823e17f1344cbccaeafe72daf9ef7c1ac298e14e
Morikiyo INOHA <morikiyo@sonicgarden.jp>
2024-11-20 12:20:03 +0900

    タスクを done に更新できる

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_task, only: %i[update]
 
-  # GET /tasks or /tasks.json
   def index
     @tasks = Task.all
   end
 
-  # GET /tasks/1 or /tasks/1.json
-  def show
-  end
-
-  # GET /tasks/new
-  def new
-    @task = Task.new
-  end
-
-  # GET /tasks/1/edit
-  def edit
-  end
-
-  # POST /tasks or /tasks.json
   def create
     @task = Task.new(task_params)
 
@@ -31,31 +16,13 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated." }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /tasks/1 or /tasks/1.json
-  def destroy
-    @task.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to tasks_path, status: :see_other, notice: "Task was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @task.update!(task_done_params)
+    redirect_to tasks_path, status: :see_other, notice: "Task was successfully updated."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_task
       @task = Task.find(params.expect(:id))
     end
@@ -63,5 +30,9 @@ class TasksController < ApplicationController
     # Only allow a list of trusted parameters through.
     def task_params
       params.expect(task: [ :title, :done ])
+    end
+
+    def task_done_params
+      params.expect(task: [ :done ])
     end
 end
